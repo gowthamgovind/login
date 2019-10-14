57f79bd871862f5f9fce72688173295562b46f5c
Rithesh Subramanian <31250867+ritheshrithu@users.noreply.github.com>
2019-10-15 02:09:40 +0530

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,10 +1,10 @@
 
   module.exports = {
-    "mongoURI": "process.env.MONGO_URI",
-    "jwtSecret": "process.env.JWT_SECRET",
-    "nexmoKey": "process.env.NEXMO_KEY",
-    "nexmoSecret": "process.env.NEXMO_SECRET",
-    "speakeasySecret": "process.env.SPEAEASY_SECRET",
-    "googleClient": "process.env.GOOGLE_CLIENT",
-    "googleSecret": "process.env.GOOGLE_SECRET"
+    "mongoURI": process.env.MONGO_URI,
+    "jwtSecret": process.env.JWT_SECRET,
+    "nexmoKey": process.env.NEXMO_KEY,
+    "nexmoSecret": process.env.NEXMO_SECRET,
+    "speakeasySecret": process.env.SPEAEASY_SECRET,
+    "googleClient": process.env.GOOGLE_CLIENT,
+    "googleSecret": process.env.GOOGLE_SECRET
   }
